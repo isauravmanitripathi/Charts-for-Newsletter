d3109d900d2d368d68d4aec757543c87901b6c2d
spmtripathi10@gmail.com <spmtripathi10@gmail.com>
2024-12-01 15:33:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AIRoadmap from './components/AIRoadmap'
+
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-gray-900">
+      <AIRoadmap />
+    </div>
+  )
+}
+
+export default App
